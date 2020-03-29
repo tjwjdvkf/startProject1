ca5e9cce88596fce81bb4142b150926a6f059cf6
Seo Jeoung Hoon <tjwjdvkf2@naver.com>
2020-03-29 17:08:42 +0900

    Initial Commit

---
+++ DiseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiseApp
+//
+//  Created by 서정훈 on 2020/03/29.
+//  Copyright © 2020 seojeounghoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
